62287c1ed27e9d0882aa94be0669342b435f1897
Dzen819 <43151543+Dzen819@users.noreply.github.com>
2021-08-12 15:27:21 +0300

    Final

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
 from question_model import Question
 from data import question_data
-
+from quiz_brain import QuizBrain
 question_bank = []
 
 for index in range(len(question_data)):
-    question = Question(question_data[index]["text"], question_data[index]["answer"])
+    question = Question(question_data[index]["question"], question_data[index]["correct_answer"])
     question_bank.append(question)
 
+quiz = QuizBrain(question_bank)
+while quiz.still_has_questions():
+    quiz.next_question()
 
+print(f"You've completed the quiz!\nYour final score was: {quiz.score}/{len(question_bank)}")
